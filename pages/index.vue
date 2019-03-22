a0994fd18f44168959a7616ef27443f0e77c48e6
alejandro delfado <desimo@hotmail.com>
2019-03-21 20:23:04 -0500

    iniciando proyecto

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>hola {{msg}}</h1>
+    <input type="text" v-model="msg">
+    <button @click="guardar">guardar</button>
+    <table>
+      <tr>
+        <th>nombre</th>
+      </tr>
+      <tr v-for="(item,index) in municipio" :key="index">
+        <td>{{item}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "alejandro delgado",
+      municipio: ["mocoa", "villagarzon", "puerto asis"]
+    };
+  },
+
+  methods:{
+    guardar() {
+     this.municipio.push(this.msg)
+     this.msg=''
+    }
+  }
+};
+</script>
