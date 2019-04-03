e440ad81d468013a344bad9f47e547b13d4cbb2a
conejo <desumo10@hotmail.com>
2019-04-03 18:13:02 -0500

    creeary porducto

--- pages/productos/crear.vue
+++ pages/productos/crear.vue
@@ -1,3 +1,47 @@
 <template>
-    <h1>desde productos crear</h1>
+  <div class="container">
+    <h1>NUEVO PRODUCTO</h1>
+    <b-from id="producto">
+      <b-form-group>
+        <label for="imagen">inserte imagen</label>
+        <b-form-file
+          v-model="file"
+          accept=".jpg, .png"
+          placeholder="inserte imagen"
+          name="imagen"
+          id="imagen"
+        ></b-form-file>
+
+        <label for="nombre">nombre</label>
+        <b-form-input id="nombre" required placeholder="ingresae nombre"></b-form-input>
+
+        <label for="precio">precio</label>
+        <b-form-input id="precio" required placeholder="pesos colombia"></b-form-input>
+
+        <label for="precio">cantidad</label>
+        <b-form-input id="cantidad" required placeholder="cuantos quiere"></b-form-input>
+      </b-form-group>
+
+      <b-button type="botton" variant="primary" @click="guardar">guardar</b-button>
+      <b-button type="botton" variant="dark">limpiar venta</b-button>
+    </b-from>
+  </div>
 </template>
+<script>
+export default {
+    methods:{
+        guardar()
+        {
+           datos.push({
+imagen: imagen.value,
+nombre: nombre.value,
+precio: precio.value,
+cantidad: cantidad.vale, 
+
+           })
+           localStorage.setItem('datos',JSON.stringify(datos))
+        }
+    }
+}
+</script>
+
--- pages/productos/index.vue
+++ pages/productos/index.vue
@@ -5,6 +5,32 @@
     </div>
     <div class="col-sm-6">
       <b-button variant="primary" href="/productos/crear">nuevo</b-button>
-    </div>
+          </div>
+          <div>
+
+  
+    <b-table striped hover :items="data"></b-table>
+  
+
+
+          </div>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+return{
+  data:[
+    {
+'id':'1', 'nombre':'conejo', 'precio':'20.000', 'cantidad':'45',
+    }
+  ]
+}
+
+  }
+
+}
+</script>
+
+
