bb9ed9515eeb44814cc9af9e618544dc5338d118
conejo <desimo10@hotmail.com>
2019-04-10 22:36:53 -0500

    guardar productos

--- pages/productos/crear.vue
+++ pages/productos/crear.vue
@@ -1,40 +1,54 @@
 <template>
   <div class="container">
     <h1>NUEVO PRODUCTO</h1>
-    <b-from id="producto">
+    <b-form @submit.prevent="guardarProducto" >
       <b-form-group>
         <label for="imagen">inserte imagen</label>
         <b-form-file  accept=".jpg, .png"
           placeholder="inserte imagen"
           name="imagen"
           id="imagen"
+    
         ></b-form-file>
 
         <label for="nombre">nombre</label>
-        <b-form-input id="nombre" required placeholder="ingresae nombre"></b-form-input>
+        <b-form-input id="nombre" v-model="form.nombre" required placeholder="ingresae nombre"></b-form-input>
 
         <label for="precio">precio</label>
-        <b-form-input id="precio" required placeholder="pesos colombia"></b-form-input>
+        <b-form-input  v-model="form.precio" id="precio" type="number" required placeholder="pesos colombia"></b-form-input>
 
         <label for="precio">cantidad</label>
-        <b-form-input id="cantidad" required placeholder="cuantos quiere"></b-form-input>
+        <b-form-input v-model="form.cantidad" id="cantidad" type="number" required placeholder="cuantos quiere"></b-form-input>
       </b-form-group>
 
-      <b-button type="botton" variant="primary" @click="guardar">guardar</b-button>
+      <b-button  variant="primary" type="submit">guardar</b-button>
       <b-button type="botton" variant="dark">limpiar venta</b-button>
-    </b-from>
+    </b-form>
   </div>
 </template>
 <script>
+import{ db } from '../../services/firebase'
+
 export default {
+  data(){
+    return{
+      form:{
+      nombre:'',
+      precio:'',
+      cantidad:''
+      }
+    }
+
+  },
   methods: {
-    guardar() {
-      datos.push({
-        imagen: imagen.value,
-        nombre: nombre.value,
-        precio: precio.value,
-        cantidad: cantidad.vale
-      });
+    guardarProducto(){
+db.collection("productos").add(this.form).then(res =>{
+this.$router.push({
+  path: "/productos"
+})
+
+})
+
     }
   }
 };
--- pages/productos/index.vue
+++ pages/productos/index.vue
@@ -1,14 +1,28 @@
 <template>
+  <div class="container mt-4">
   <div class="row mt-3">
     <div class="col-sm-6">
       <h2>listado de productos</h2>
     </div>
-    <div class="col-sm-6">
+    <div class="col-sm-6 text-right">
       <b-button variant="primary" href="/productos/crear">nuevo</b-button>
     </div>
-    <div>
-      <b-table responsive striped hover :fields="fields" :items="productos"></b-table>
-    </div>
+  </div>
+    <div class="overflow-auto">
+      <b-table responsive striped hover :fields="fields" :items="productos" id="lista_productos" :per-page="perPage" :current-page="currentPage">
+
+ 
+      </b-table>
+ <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+       aria-controls="lista_productos"
+    ></b-pagination>
+
+    
+    
+  </div>
   </div>
 </template>
 
@@ -20,13 +34,16 @@ export default {
   asyncData() {
     return db.collection("productos").get().then((productosSnap) => {
         let productos = []
+
         productosSnap.forEach((value) => {
           
           productos.push(value.data())
         })
         
         return {
-          productos
+          productos,
+          currentPage:1,
+          perPage: 8
         }
       })
   },
@@ -35,6 +52,11 @@ export default {
       fields: ["imagen", "nombre", "precio", "cantidad", "acciones"]
     }
   },
+  computed:{
+    rows(){
+      return this.productos.length
+    }
+  }
 }
 
 </script>
